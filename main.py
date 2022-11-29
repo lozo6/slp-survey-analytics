65562b08c313d0240305c658f7c63d133d43180c
lozo6 <99729303+lozo6@users.noreply.github.com>
2022-11-28 20:45:32 -0500

    tests

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 df = pd.read_csv('data/englishSLP.csv')
+
+df.shape
+
+df.info()
+
+df.columns()
